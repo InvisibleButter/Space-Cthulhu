1caec3d43fdff75cf4174dc0e8a3cb9e915aed8d
Maxie Bernt <maxie.bernt@hotmail.de>
2020-07-11 20:10:37 +0200

    ADDED nav-mash-stuff and following player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,28 @@
-﻿public class Enemy : Entity
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : Entity
 {
+    private NavMeshAgent _navMashAgent;
+    private Transform _target;
+    private float _updateTime = 0.5f;
     protected override void Start()
     {
         base.Start();
+        _navMashAgent = GetComponent<NavMeshAgent>();
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        StartCoroutine(RefreshTargetPos());
+    }
+
+    private IEnumerator RefreshTargetPos()
+    {
+        while(_target != null)
+        {
+            _navMashAgent.SetDestination(_target.position);
+            yield return new WaitForSeconds(_updateTime);
+        }
     }
 
     public override void Die()
